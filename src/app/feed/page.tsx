12201defaefa60895a5224c1bd6bdfc98dcf1ee6
wasem-ahmmd <wasemahmmd@gmail.com>
2024-06-13 03:37:12 -0700

    some bug fix in main body

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -8,14 +8,14 @@ export default function feed() {
       <>
         <Header />
         <MainSideBar />
-        <main className="xxl:ml-side  xl:ml-side-sm py-2.5 xl:px-10 px-2  h-[calc(100vh-var(--m-top))] mt-top-custom">
+        <main className="xxl:ml-side  xl:ml-side-sm py-2.5 xl:pl-10 px-4  h-[calc(100vh-var(--m-top))] mt-top-custom">
           <div className="lg:flex xxl:gap-16 gap-12 max-w-[1065px] mx-auto">
             <div className="max-w-[680px] mx-auto">
               <div className="md:max-w-[580px] mx-auto flex-1 xl:space-y-6 space-y-3">
                 <FeedUI />
               </div>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 lg:pl-10">
               <div className="lg:space-y-4 lg:pb-8 max-lg:grid sm:grid-cols-2 max-lg:gap-6 uk-sticky uk-active uk-sticky-fixed">
                 <PeopleKnow />
               </div>
