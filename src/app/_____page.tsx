81203375063b4830d56cec63104922391e4f8533
wasem-ahmmd <wasemahmmd@gmail.com>
2024-06-23 13:34:33 -0700

    some bix fix

--- src/app/_____page.tsx
+++ src/app/_____page.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/common/headers/Header";
 import MainSideBar from "@/components/common/sidebars/MainSideBar";
-import { sideBarlink } from "@/data/Static";
+// import { sideBarlink } from "@/data/Static";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <MainSideBar>
+      {/* <MainSideBar>
         {sideBarlink?.map((item, index) => (
           <Link href={item.link} key={item.title}>
             <div className="flex items-center gap-4 px-4 py-2 rounded-xl w-auto hover:bg-BG-4 mb-1 cursor-pointer">
@@ -16,7 +16,7 @@ export default function Home() {
             </div>
           </Link>
         ))}
-      </MainSideBar>
+      </MainSideBar> */}
       <main className="xxl:ml-side  xl:ml-side-sm p-2.5 h-[calc(100vh-var(--m-top))] mt-top-custom">
         <Link href={"/login"}>login</Link>
         <Link href={"/signup"}>signup</Link>
